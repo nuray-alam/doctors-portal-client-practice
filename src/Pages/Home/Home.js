c331f5bb5f4a405dd9a6c6ab4bad522973fd4287
nuray-alam <siddik4x@gmail.com>
2022-11-07 12:51:11 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-white'>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;

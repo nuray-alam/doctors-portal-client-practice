74a672b16ad1973beb0b71040eb1a7179df72eb8
nuray-alam <siddik4x@gmail.com>
2022-11-25 22:47:58 +0600

    user specific appointments table added to dashboard

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppointments = () => {
+    const [user] = useAuthState(auth);
+    const [appointments, setAppointments] = useState([]);
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointments(data))
+        }
+
+
+    }, [user])
+
     return (
         <div>
-            <h1 className='text-4xl'>My Appointments</h1>
+            <h1 className='text-4xl'>My Appointments {appointments.length}</h1>
+            <div className="overflow-x-auto bg-white">
+                <table className="table w-full bg-white">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th className='bg-white'></th>
+                            <th className='bg-white'>Name</th>
+                            <th className='bg-white'>Date</th>
+                            <th className='bg-white'>Time</th>
+                            <th className='bg-white'>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        {
+                            appointments.map(a => <tr>
+                                <th className='bg-white'>1</th>
+                                <td className='bg-white'>{a.patientName}</td>
+                                <td className='bg-white'>{a.date}</td>
+                                <td className='bg-white'>{a.slot}</td>
+                                <td className='bg-white'>{a.treatment}</td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -34,12 +34,18 @@ const Navbar = () => {
                         {menuItems}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-2xl font-bold">Doctors Portal</a>
+                <Link to='/' className="btn btn-ghost normal-case text-2xl font-bold">Doctors Portal</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
                     {menuItems}
                 </ul>
+            </div>
+            <div className="navbar-end">
+                <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                {/* <label htmlFor="dashboard-sidebar" className="btn btn-primary drawer-button lg:hidden">Open drawer</label> */}
             </div>
         </div>
     );

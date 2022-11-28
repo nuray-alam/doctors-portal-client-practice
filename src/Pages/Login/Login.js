1f60c6059df7810333420fe0a01d2cd7f9629c61
nuray-alam <siddik4x@gmail.com>
2022-11-28 20:53:06 +0600

    jwt token added from the server side

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 
 const Login = () => {
@@ -19,6 +20,8 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+const [token] = useToken(user || gUser)
+
     let signInError;
 
     const navigate = useNavigate();
@@ -27,11 +30,11 @@ const Login = () => {
 
     useEffect(() => {
 
-        if (user || gUser) {
+        if (token) {
             navigate(from, { replace: true });
         }
 
-    }, [user, gUser, from, navigate])
+    }, [token, from, navigate])
 
     if (gLoading || loading) {
 
